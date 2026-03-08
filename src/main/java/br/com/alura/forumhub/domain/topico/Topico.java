fc9b6a223d906cf8ad442750c815796b35c80612
João Paulo <jhon001.jp@gmail.com>
2026-03-08 15:17:50 -0300

    feat: Finalizando CRUD de tópicos com segurança JWT

---
+++ src/main/java/br/com/alura/forumhub/domain/topico/Topico.java
@@ -0,0 +1,45 @@
+package br.com.alura.forumhub.domain.topico;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+
+    private String autor;
+    private String curso;
+
+    public Topico(DadosCadastroTopico dados) {
+        this.titulo = dados.titulo();
+        this.mensagem = dados.mensagem();
+        this.autor = dados.autor();
+        this.curso = dados.curso();
+        this.status = StatusTopico.NAO_RESPONDIDO;
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoTopico dados) {
+        if (dados.titulo() != null) {
+            this.titulo = dados.titulo();
+        }
+        if (dados.mensagem() != null) {
+            this.mensagem = dados.mensagem();
+        }
+    }
+}
